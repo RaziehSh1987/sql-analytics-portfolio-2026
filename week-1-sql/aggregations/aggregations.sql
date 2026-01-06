a3160a77894ad236e0d6bc449a3709e21fecd5f1
raziehShahsavar <82935599+RaziehSh1987@users.noreply.github.com>
2026-01-06 13:12:44 -0800

    groupby cross join order by

--- week-1-sql/aggregations/aggregations.sql
+++ week-1-sql/aggregations/aggregations.sql
@@ -1,1 +1,24 @@
--- Day X practice will go here
+-- Day 2 — -- Aggregation with global KPIs using CTE + CROSS JOIN
+
+-- Business question:
+-- Total number of customers
+-- Number of unique countries
+-- Number of customers per country
+
+
+with stats as(
+	select 
+		count(*) as total_country,
+		count(distinct country) as unique_country
+	from customers
+)
+select 
+	c.country,
+	count(customer_id) as customer_count,
+	s.total_country,
+	s.unique_country
+from  customers c
+cross join stats s
+group by c.country , s.total_country , s.unique_country
+order by customer_count desc;
+
