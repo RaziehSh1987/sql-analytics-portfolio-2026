46a7178392533b6e5caed6bdbb9977dd173a3707
raziehShahsavar <82935599+RaziehSh1987@users.noreply.github.com>
2026-01-05 23:48:48 -0800

    Update window_functions.sql

--- week-1-sql/window_function/window_functions.sql
+++ week-1-sql/window_function/window_functions.sql
@@ -2,17 +2,6 @@
 -- Business question:
 -- How many customers from Canada signed up in the last 6 months?
 
--- Example 1: Basic filtering
-SELECT name, signup_date
-FROM customers
-WHERE country = 'Canada'
-  AND signup_date >= '2024-01-01';
-
--- Example 2: Count customers (dynamic date)
-SELECT COUNT(*) AS canada_last_6_months
-FROM customers
-WHERE country = 'Canada'
-  AND signup_date >= CURRENT_DATE - INTERVAL '6 months';
 
 -- Example 3: Defensive / production-safe version
 -- Handles casing and extra spaces in country field
